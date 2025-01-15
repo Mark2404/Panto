60ed18f0ee10a8cf135775c73dc6c3a48699f663
User <markpomidorchik@gamil.com>
2025-01-15 19:53:26 +0500

    done

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const buttons = document.querySelectorAll('.fl-buttons');
 const counterElement = document.getElementById('counter');
 const plusBtn = document.querySelectorAll('.plus-btn')
 let count = 0;
-
-
+const all = document.getElementById('all')
+const sec = document.getElementById('hidden-sec')
+const hide = document.getElementById('hide')
+const buttonBox = document.getElementById('buttonBox')
+const buttonBox1 = document.getElementById('buttonBox1')
 buttons.forEach(button => {
     button.addEventListener('click', () => {
 
@@ -27,3 +30,20 @@ plusBtn.forEach(button => {
         }
     });
 });
+
+all.addEventListener('click', () => {
+    sec.classList.add('not-hidden')
+    sec.classList.remove('hidden')
+    buttonBox1.classList.add('display-none')
+    buttonBox1.classList.remove('display-block')
+    buttonBox.classList.remove('display-none')
+    buttonBox.classList.add('display-block')
+});
+hide.addEventListener('click', () => {
+    sec.classList.remove('not-hidden')
+    sec.classList.add('hidden')
+    buttonBox.classList.remove('display-block')
+    buttonBox.classList.add('display-none')
+    buttonBox1.classList.add('display-block')
+});
+
